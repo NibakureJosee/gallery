58b28fd766b91ff5121e36c18e0fc4e3e14510d2
NibakureJosee <nibakurejose@gomail.com>
2022-11-14 07:02:54 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
      <h1 style={{textAlign: 'center'}}>Image gallery</h1>
-     <h2 style={{textAlign: 'center'}}>Love is what make us happy even when we are sad</h2>
+     <h2 style={{textAlign: 'center'}}>Keep your face always toward the sunshine, and shadows will fall behind you</h2>
      <Gallery />
     </>
   );
